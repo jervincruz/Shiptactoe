a0c49e3b0a28fa9c58a5612f294c7503509ba9f9
Jervin Cruz <jervincruz26@gmail.com>
2018-06-15 08:35:02 -0700

    Add more features to newGame method. Make all variables reset (board, currentPlayer, and moves).

--- Shiptactoe/ViewController.swift
+++ Shiptactoe/ViewController.swift
@@ -15,15 +15,7 @@ class ViewController: UIViewController {
     var currentPlayer = 1 // Player 1 = X
     var board : [Int] = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
     let combos : [[Int]] = [ [0,3,6], [1,4,7], [2,5,8], [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6] ]
-//    //let combos : [Int : [[Int]]] = [0 : [ [1,2], [3,6], [4,8] ],
-//                                    1 : [ [0,2], [4,7] ],
-//                                    2 : [ [0,1], [5,8], [4,6] ],
-//                                    3 : [ [0,6], [4,5] ],
-//                                    4 : [ [0,8], [2,6], [3,5], [1,7] ],
-//                                    5 : [ [2,8], [3,4] ],
-//                                    6 : [ [0,3], [2,4], [7,8]],
-//                                    7 : [ [1,4], [6,8]],
-//                                    8 : [ [0,4], [2,5], [6,7]]]
+    
     let alertTitle : String = "W I N N E R"
     let drawTitle : String = "D R A W"
     let message1 = "Player 1 wins!"
@@ -36,8 +28,8 @@ class ViewController: UIViewController {
     
     @IBAction func moveMade(_ sender: UIButton) {
         
-        var tag = sender.tag - 1
-        board[tag] = currentPlayer
+        let tag = sender.tag - 1
+        board[tag] = currentPlayer // Assign "1" or "2" to board
         
         if currentPlayer == 1 {
             sender.setImage(UIImage(named: "X"), for: UIControlState())
@@ -47,7 +39,7 @@ class ViewController: UIViewController {
             currentPlayer = 1
         }
     
-        
+        // Solution
         for combo in combos {
             if board[combo[0]] != -1 // Not Empty Board
             && board[combo[0]] == board[combo[1]] // 1st matches 2nd spot
@@ -67,7 +59,8 @@ class ViewController: UIViewController {
                         self.newGame((Any).self)
                     }
                     winAlert.addAction(action2)
-                    self.present(winAlert, animated: true, completion: nil)                }
+                    self.present(winAlert, animated: true, completion: nil)
+                }
             }
         }
         
@@ -83,7 +76,6 @@ class ViewController: UIViewController {
             drawAlert.addAction(action1)
             self.present(drawAlert, animated: true, completion: nil)
         }
-        
         print(board)
     }
     
@@ -93,6 +85,11 @@ class ViewController: UIViewController {
             button.isUserInteractionEnabled = true
             button.setImage(nil, for: .normal)
         }
+        
+        // Reset All Variables
+        board = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+        currentPlayer = 1
+        moves = 0
     }
     
 }
